6e25971d95aecdff3a0929c2f8f55cb5e751cc36
neojou <neojou@gmail.com>
2018-12-09 16:12:10 +0800

    linear regression

---
+++ Day7/linear.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('studentscores.csv')
+X = dataset.iloc[ :, : 1].values
+Y = dataset.iloc[ : , 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/4, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor = regressor.fit(X_train, Y_train);
+
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.show()
+
